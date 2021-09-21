86caddec28b36ffd39bbcfc76034be4ec50fb13b
Marko Johnson <MarkoJohnson@QuickenLoans.com>
2021-09-20 21:56:34 -0400

    first commit

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> results = DAL.GetAllProducts();
+            return View(results);
+        }
+
+        public IActionResult product(int prod)
+        {
+            Product theprod = DAL.GetProduct(prod);
+            return View(theprod);
+        }
+    }
+}
---
+++ Models/DAL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+
+namespace CoffeeShop.Models
+{
+    public class DAL
+    {
+        // Database connection
+        // make sure that we are "using MySql.Data.MySqlClient;"
+        public static MySqlConnection DB = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=abc123");
+
+
+        public static List<Product> GetAllProducts()
+        {
+            return DB.GetAll<Product>().ToList();
+        }
+
+        public static Product GetProduct(int id)
+        {
+            return DB.Get<Product>(id);
+        }
+    }
+
+
+}
